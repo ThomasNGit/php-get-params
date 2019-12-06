e589de2b1b6f6b8905cef850b20bc700277f762f
ThomasNGit <neronithomas1989@gmail.com>
2019-12-06 13:40:45 +0100

    aggiunto codice che permette di determinare se la password(passato col GET) è "Boolean" e stampa una stringa verde in pagina, in caso contrario stampa una scritta rossa.
    Esercizio terminato

--- index.php
+++ index.php
@@ -3,8 +3,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Prova php</title>
+    <title>Password Check</title>
 <?php 
+
+$pwd = $_GET['password'];
 
 
 ?>
@@ -13,6 +15,11 @@
 
 <?php 
 
+if($pwd != "boolean"){
+    echo "<h1 style='color:red'> PASSWORD ERRATA! </h1>";
+} else {
+    echo "<h1 style='color:green'> PASSWORD CORRETTA! </h1>";
+}
 ?>
 
 
